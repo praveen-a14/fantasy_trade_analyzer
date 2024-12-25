e75fc82e9ba180796cc8d46e1bc6dae1c6f7b442
praveen-a14 <136651867+praveen-a14@users.noreply.github.com>
2024-12-24 22:43:03 -0800

    Update trades.py

--- trades.py
+++ trades.py
@@ -4,10 +4,12 @@ import os
 import json
 import streamlit as st
 from collections import defaultdict
+import nfl_data_py as nfl
 
 # Define file paths
 player_data_file = "players_data.json"
 transaction_data_file = "transactions_data.json"
+stats_data_file = "stats_data.json"
 
 urls = {
         2020: "https://api.sleeper.app/v1/league/634178924393340928/",
@@ -53,14 +55,49 @@ def load_transaction_data():
                 url = f"{urls.get(year)}transactions/{week}"
                 try:
                     response = requests.get(url)
-                    response.raise_for_status()  # Will raise an HTTPError if the HTTP request returned an unsuccessful status code
+                    response.raise_for_status()
                     data[str(year)].extend(response.json())
                 except requests.RequestException as e:
                     st.error(f"An error occurred while fetching transactions for {year}, week {week}: {e}")
         
         with open(transaction_data_file, "w") as file:
             json.dump(data, file)
-    
+    return data
+
+@st.cache_data
+def load_stats_data():
+    if os.path.exists(stats_data_file):
+        with open(stats_data_file, "r") as file:
+            data = json.load(file)
+    else:
+        # Load data from nfl_data_py
+        stats = nfl.import_weekly_data(
+            [2020, 2021, 2022, 2023, 2024],
+            ['player_id', 'player_display_name', 'position', 'season', 'season_type', 'week', 'fantasy_points_ppr']
+        )
+        stats = stats[stats['season_type'] == 'REG']
+        ids = nfl.import_ids(['gsis_id', 'sleeper_id'])
+        data = pd.merge(
+            stats, ids,
+            left_on='player_id', right_on='gsis_id',
+            how='inner'
+        )[['player_id', 'position', 'player_display_name', 'season', 'week', 'fantasy_points_ppr', 'sleeper_id']]
+
+        # Convert to a list of dictionaries
+        data = data.to_dict(orient="records")
+        
+        """  # Group by sleeper_id
+        grouped_data = {}
+        for entry in data:
+            sleeper_id = entry['sleeper_id']
+            if sleeper_id not in grouped_data:
+                grouped_data[sleeper_id] = []
+            grouped_data[sleeper_id].append(entry) """
+
+        # Write grouped_data to file
+        with open(stats_data_file, "w") as file:
+            json.dump(data, file)
+
     return data
 
 @st.cache_data
@@ -103,16 +140,12 @@ st.title("Machiavelli Trade Viewer")
 # Load player data
 players = load_player_data()
 transactions = load_transaction_data()
+stats = load_stats_data()
 
-# Show info about player data
-st.sidebar.header("Player Data")
-st.sidebar.text(f"Total Players: {len(players)}")
-
-# Get Year and Week from User
+# Get Year and Team from User
 year = st.sidebar.selectbox("Select Year", [2020, 2021, 2022, 2023, 2024])
 team = st.sidebar.selectbox("Select Team", ["Beckham", "Tyler", "Praveen", "Andre", "Jonny", "Gov", "Nick", "Cameron", "Joseph", "Kai/Arshon/Stathis", "Chase/Tin", "Robert/Ryan"])
 names_dict = {"Beckham": ['brazybabybc', 'bc5934'], "Tyler": ['norris13', 'JoeBrownFanClub'], "Praveen": ['praveen14'], "Andre": ['sheluvgov', 'chicosman'], "Jonny": ['AndreRishel', 'TeamJonnyL'], "Gov":['GovsForeskin', 'Govvy'], "Nick": ['BucklingRelic12'], "Cameron": ['PuffDad'], "Joseph": ['SuperVUsters'], "Kai/Arshon/Stathis": ['RatchetRabies', 'guccigaropppp', 'TheStinkers'], "Chase/Tin": ['chade1', 'Matinnn'], "Robert/Ryan": ['GuapGetterz999', 'Br0wnsBunch', 'TheDanesh30']}
-#week = st.sidebar.selectbox("Select Week", list(range(1, 19)))
 
 # Get Rosters and Users for that Year
 rosters = get_rosters(year=year)
@@ -120,11 +153,9 @@ users = get_users(year=year)
 
 user_dict = {user['user_id']: user['display_name'] for user in users if 'display_name' in user}
 
-# Display transactions
-#transactions = get_transactions(year=year, week=week)
-
 for transaction in transactions[str(year)]:
     if transaction['type'] == 'trade':
+        transaction_year = year
         # Check if the selected team is involved in the transaction
         involved_teams = [roster['owner_id'] for roster in rosters if roster['roster_id'] in transaction['roster_ids']]
         involved_team_names = [user_dict.get(owner, f"Unknown User ({owner})") for owner in involved_teams]
@@ -145,24 +176,70 @@ for transaction in transactions[str(year)]:
             
             # Combine players and draft picks into one structure per team
             trade_details = {}
+            team_season_total = 0
+            team_total = 0
+            player_info = {}
+            for player in stats:
+                sleeper_id = player['sleeper_id']
+                if sleeper_id not in player_info:
+                    player_info[sleeper_id] = []
+                player_info[sleeper_id].append(player)
 
             # Show added players and draft picks in the trade
             if transaction['adds']:
+                # Assuming stats is a dictionary of lists of dictionaries
                 for player_id, roster_id in transaction['adds'].items():
-                    if player_id in players.index: 
-                        player_info = players.loc[player_id] 
+                    player_first_season_total = 0
+                    not_first_season_total = 0
+                    player_total = 0
+                    first_season_weeks = 1
+                    first_season_weeks = 0
+                    not_first_season_weeks = 0
+                    # stats is a list of dicts instead of a dict of lists of dicts
+                    if player_id.isdigit(): 
+                        # If player_id is numeric (indicating a player)
+                        player = player_info.get(float(player_id), [])
+                            
+                    if player:
+                        # Player found in stats, you can proceed with your logic here
+                        for week in player:  # Assuming player_info contains a list of weeks
+                            if (week['season'] == transaction_year) and (week['week'] >= transaction['leg']):
+                                player_first_season_total += week['fantasy_points_ppr']
+                                first_season_weeks += 1
+                            elif week['season'] > transaction_year:
+                                not_first_season_total += week['fantasy_points_ppr']
+                                not_first_season_weeks += 1
+                        
+                        total_weeks = first_season_weeks + not_first_season_weeks
+                        if total_weeks < 1:
+                            total_weeks = 1
+                        player_total = player_first_season_total + not_first_season_total
+                        total_ppg = player_total / total_weeks
+                        total_ppg = round(total_ppg, 2)
+                        if first_season_weeks < 1:
+                            first_season_weeks = 1
+                        first_season_ppg = player_first_season_total / first_season_weeks
+                        first_season_ppg = round(first_season_ppg, 2)
+                        if not_first_season_weeks < 1:
+                            not_first_season_weeks = 1
+                        not_first_season_ppg = not_first_season_total / not_first_season_weeks
+                        not_first_season_ppg = round(not_first_season_ppg, 2)
+                        # Get the roster owner from the rosters list
                         roster = next((r for r in rosters if r['roster_id'] == roster_id), None)
                         if roster:
-                            owner_id = roster['owner_id'] 
-                            display_name = user_dict.get(owner_id, f"Unknown User ({owner_id})") 
-
+                            owner_id = roster['owner_id']
+                            display_name = user_dict.get(owner_id, f"Unknown User ({owner_id})")
+                            
+                            # Update trade details for the roster
                             if roster_id not in trade_details:
                                 trade_details[roster_id] = {"team": display_name, "items": []}
                             
-                            trade_details[roster_id]["items"].append(f"{player_info['first_name']} {player_info['last_name']} ({player_info['position']})")
+                            trade_details[roster_id]["items"].append(f"{player[0].get('player_display_name')} ({player[0].get('position')}) - Rest of Season: {first_season_ppg} PPG ({first_season_weeks} Gs), Future Seasons: {not_first_season_ppg} PPG ({not_first_season_weeks} Gs)")
                     else:
+                        # Player not found in stats
                         log_output += f"Player ID {player_id} not found in player data.\n"
 
+                                
             if transaction['draft_picks']:
                 for pick in transaction['draft_picks']:
                     original = next((r for r in rosters if r['roster_id'] == pick['roster_id']), None)
@@ -200,7 +277,7 @@ for transaction in transactions[str(year)]:
 st.markdown(""" 
 <style>
 .st-dd {
-    -webkit-text-fill-color: white;
+    -webkit-text-fill-color: white !important;
 }
 textarea {
     font-size: 20px !important;
